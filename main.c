599058effa77698140ee4877fcc8d87d5dc7c827
Gogoonbuntu <tmddud333@naver.com>
2020-03-20 07:38:35 +0000

    almost done

--- main.c
+++ main.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <memory.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define LEFT(i) (2*i)
+#define RIGHT(i) (LEFT(i)+1)
 
 int insertf(void);
 int deletef(void);
 int retrf(void);
 int keyf(void);
 int printAllf(void);
+void max_heapify(int* a, int b);
+int max(int* a);
+void swap(int* a, int b, int c);
+void strcpyf(char* b, char* a);
 
+int keys[10]; //heap array
+char names[10][30];
+int size = 0;
+int i, j;
+char menu;
 
 int main(void) {
-	char menu;
 	while(1){
-		printf("\n*********** MENU ***********\n");
+		menu = '\0';
+		printf("\n\n*********** MENU ***********\n");
 		printf("I : Insert new element into queue\n");
 		printf("D : Delete element with largest key from queue.\n");
 		printf("R : Retrieve element with largest key from queue\n");
-		printf("K : Increase key of elecment in queue.");
+		printf("K : Increase key of elecment in queue.\n");
 		printf("P : Print out all elements in queue.\n");
 		printf("Q : Quit\n\n");
+
 		printf("Choose menu :  ");
-		scanf("%c", &menu);
+		scanf(" %c", &menu);
+		getchar();
 		menu = tolower(menu);
+		
 		switch(menu) {
-			case 'i' : insertf();
-			case 'd' : deletef();
-			case 'r' : retrf();
-			case 'k' : keyf();
-			case 'p' : printAllf();
+			default: break;
+			case 'i' : insertf();break;
+			case 'd' : deletef();break;
+			case 'r' : retrf();break;
+			case 'k' : keyf();break;
+			case 'p' : printAllf();break;
 			case 'q' : return 0;
-			default: continue;
 		}
 	}
 }
@@ -40,19 +58,112 @@ int insertf(void){
 	scanf("%s", name);
 	printf("Enter key value of element: ");
 	scanf("%d", &keyv);
-	printf("\nNew element [%s, %d] is inserted", name, keyv);
+	
+	i=1;
+	while(keys[i]!='\0'){
+		if(keyv==keys[i]){
+			printf("key already exist.\n");
+			return 0;
+		}
+		i++;
+	}
+	size++;
+	keys[size]=keyv;
+	
+	strcpyf(names[keyv], name);
+	//printf("keyv : %d, value : %s\n", keys[size], names[keyv]);
+	printf("\nNew element [%s, %d] is inserted\n", name, keyv);
+	if(size>1)
+		for(i=size/2; i>0; i--){
+			max_heapify(keys, i);
+		}
+
+	return 0;
 }
 
 int deletef(void){
-	char name[30];
+	char name[30]="";
 	int keyv;
-	printf("\n[%s, %d] is deleted", name, keyv);
+	keyv = keys[1];
+	strcpyf(name, names[keys[1]]);
+	int len = (int)strlen(name);
+	for(i=1;i<=size;i++){
+		swap(keys, i, i+1);
+	}
+	int t = keys[size+1];
+	strcpy(names[t],"");
+	keys[size+1]='\0';
+	printf("\n[%s, %d] is deleted\n", name, keyv);
+}
+
+int retrf(void){
+	int t = keys[1];
+	printf("\n[%s, %d]\n", names[t], t);
 }
 
 int keyf(void){
-
+	printf("\nEnter index of element: ");
+	
+	printf("\nEnter new key value: ");
 }
 
 int printAllf(void){
+	i=1;
+	while(1){
+		if(keys[i]!='\0'){
+			printf("[");
+			printf("%s, %d", names[keys[i]], keys[i]);
+			printf("]");
+			i++;
+		} else {
+			return 0;
+		}
+	}
+}
 
+void max_heapify(int* keys, int q){
+	int l, r;
+	int largest = q;
+	l = LEFT(q);
+	r = l + 1;
+
+	
+	if(l<=size && keys[l] > keys[q]){
+		largest = l;
+	}
+
+	if(r<=size && keys[r] > keys[largest]){
+		largest = r;
+	}
+
+	if(largest != q){
+		swap(keys, q, largest);
+		max_heapify(keys, largest);
+	}
+}
+
+int max(int* a) {
+	i=1;
+	int maxn = a[i];
+	while(a[i]!='\0'){
+		i++;
+		a[i]>maxn ? maxn=a[i] : maxn;
+	}
+	return maxn;
+}
+
+void swap(int* a, int b, int c){
+	printf("swap!\n");
+	int t;
+	t = a[b];
+	a[b] = a[c];
+	a[c] = t;
+}
+
+void strcpyf(char* b, char* a) {
+	i=0;
+	while(a[i]!='\0'){
+		b[i] = a[i];
+		i++;
+	}
 }
